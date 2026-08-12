b6077f3319cb5a05af495b033e9d44873df7dd50
PoiTaro <takesi0603+point@icloud.com>
2026-08-12 14:18:40 +0900

    Upgrade battle BGM arrangement

--- tests/audio-system.test.js
+++ tests/audio-system.test.js
@@ -7,6 +7,12 @@ const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
 assert.match(html, /const gameAudio = \(\(\) =>/);
 assert.match(html, /api\.startBgm\s*=\s*\(\)\s*=>/);
 assert.match(html, /api\.sfx\s*=\s*\(name,opts=\{\}\)\s*=>/);
+assert.match(html, /bgmStepDuration:60\/128\/4/);
+assert.match(html, /api\._chord=\(notes,when\)=>/);
+assert.match(html, /api\._kick=\(when,accent=1\)=>/);
+assert.match(html, /api\._snare=\(when,accent=1\)=>/);
+assert.match(html, /api\._hat=\(when,open=false\)=>/);
+assert.match(html, /api\.bgmStep\+\+%128/);
 assert.match(html, /AudioContext\|\|window\.webkitAudioContext/);
 for (const event of ['shoot', 'sub', 'explode', 'jump', 'kill', 'death', 'special', 'countdown', 'matchEnd']) {
   assert.match(html, new RegExp("case ['\\\"]" + event + "['\\\"]"));
