24e6ada1c196aa301986ac04f2adfb7312970325
PoiTaro <takesi0603+point@icloud.com>
2026-08-12 12:57:48 +0900

    Simplify stage intro overlay

--- tests/stage-intro.test.js
+++ tests/stage-intro.test.js
@@ -7,6 +7,8 @@ const html = fs.readFileSync(path.join(__dirname, '..', 'index.html'), 'utf8');
 assert(/id="stageIntro"/.test(html), 'stage intro overlay must be present');
 assert(/function beginStageIntro\(startAt=Date\.now\(\)\)/.test(html), 'stage intro must support a synchronized start time');
 assert(/function introCameraPose\(elapsed\)/.test(html), 'stage intro must animate a camera path');
+assert(/class="stageIntroName"/.test(html), 'stage intro must expose only the edge stage-name label');
+assert(!/stageIntroProgressFill|stageIntroCard|stageIntroHint/.test(html), 'stage intro must not render a progress bar or instructional card');
 assert(/function finishStageIntro\(\)/.test(html) && /camera\.fov=stageIntroPrevFov/.test(html), 'intro must restore gameplay camera state');
 assert(/if \(stageIntroActive\) updateStageIntro\(\)/.test(html), 'intro animation must run outside the gameplay loop');
 assert(/startMatch\(m\.startAt\|\|Date\.now\(\)\)/.test(html), 'online guests must use the host start timestamp');
